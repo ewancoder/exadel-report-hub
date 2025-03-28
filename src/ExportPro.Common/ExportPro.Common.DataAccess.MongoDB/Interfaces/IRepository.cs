94d60b0a3196bb5c666a1fe8bf141405a931a6f0
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-03-28 17:50:11 +0400

    remove http files, add methods to repos class

--- src/ExportPro.Common/ExportPro.Common.DataAccess.MongoDB/Interfaces/IRepository.cs
+++ src/ExportPro.Common/ExportPro.Common.DataAccess.MongoDB/Interfaces/IRepository.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using ExportPro.Common.Models.MongoDB;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
 namespace ExportPro.Common.DataAccess.MongoDB.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<TDocument> where TDocument : IModel
     {
+        Task AddOneAsync<TDocument>(TDocument entity, CancellationToken cancellationToken);
+        Task AddManyAsync<TDocument>(ICollection<TDocument> entities, CancellationToken cancellationToken);
+        Task UpdateOneAsync<TDocument>(TDocument entity, CancellationToken cancellationToken);
+        Task UpdateManyAsync<TDocument>(ICollection<TDocument> entities, CancellationToken cancellationToken);
+        Task GetByIdAsync<TDocument>(ObjectId id, CancellationToken cancellationToken);
+        Task GetOneAsync<TDocument>(Expression<Func<TDocument, bool>> filter, CancellationToken cancellationToken);
 
     }
 }
