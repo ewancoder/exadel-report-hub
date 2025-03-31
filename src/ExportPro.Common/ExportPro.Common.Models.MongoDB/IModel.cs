b64fdccb8fd5080f46329e71f35ec7aac73e8c80
GiorgiUer <guffera2005@gmail.com>
2025-03-31 14:16:13 +0400

    Added Repositories

---
+++ src/ExportPro.Common/ExportPro.Common.Models.MongoDB/IModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ExportPro.Common.Models.MongoDB
+{
+   public interface IModel
+    {
+        ObjectId Id { get; set; }
+    }
+}
