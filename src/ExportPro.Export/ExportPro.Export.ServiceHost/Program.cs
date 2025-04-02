4a61d63c92e7dede2e11a11e24f2e75b0626ecd6
BachiMjavanadze <mjavanadzebachi@mail.com>
2025-04-02 13:04:21 +0400

    add swagger support

--- src/ExportPro.Export/ExportPro.Export.ServiceHost/Program.cs
+++ src/ExportPro.Export/ExportPro.Export.ServiceHost/Program.cs
@@ -4,26 +4,21 @@ using ExportPro.Common.DataAccess.MongoDB.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<IMongoDbConnectionFactory, MongoDbConnectionFactory>();
 builder.Services.AddScoped<ExportProMongoContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
