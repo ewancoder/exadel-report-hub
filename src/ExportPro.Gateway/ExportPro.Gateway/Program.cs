1193a6363cebbd3c30951bfa7b96de288c587752
BachiMjavanadze <mjavanadzebachi@mail.com>
2025-04-02 16:07:29 +0400

    fix AuthController

--- src/ExportPro.Gateway/ExportPro.Gateway/Program.cs
+++ src/ExportPro.Gateway/ExportPro.Gateway/Program.cs
@@ -1,17 +1,21 @@
+using ExportPro.Gateway.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerServices();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
