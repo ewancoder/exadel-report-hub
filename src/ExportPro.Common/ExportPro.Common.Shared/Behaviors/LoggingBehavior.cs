b48a8f59a5ac2b71b0f177c4e5e9039c0b09d7f9
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-04-03 19:06:59 +0400

    suggestion changes

--- src/ExportPro.Common/ExportPro.Common.Shared/Behaviors/LoggingBehavior.cs
+++ src/ExportPro.Common/ExportPro.Common.Shared/Behaviors/LoggingBehavior.cs
@@ -1,27 +1,17 @@
-﻿
-
-using ExportPro.Common.Shared.Library;
+﻿using ExportPro.Common.Shared.Library;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
 namespace ExportPro.Common.Shared.Behaviors;
 
-public class LoggingBehavior<TRequest, TResponse> 
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest: IRequest<TResponse>
     where TResponse: BaseResponse
 {
 
-    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
-    //TODO gonna need this when we have built auth
-    //private readonly IHttpContextAccessor _contextAccessor;
-
-    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger = logger;
 
     public async Task<TResponse> Handle(TRequest request, 
             RequestHandlerDelegate<TResponse> next, 
