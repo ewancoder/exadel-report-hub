918858c465bdeba8ed303ec93f32c636fe7d5320
BachiMjavanadze <mjavanadzebachi@mail.com>
2025-04-07 09:24:50 +0400

    return RefreshToken on login

--- src/ExportPro.Auth/ExportPro.Auth.SDK/DTOs/AuthResponseDto.cs
+++ src/ExportPro.Auth/ExportPro.Auth.SDK/DTOs/AuthResponseDto.cs
@@ -5,7 +5,6 @@ namespace ExportPro.Auth.SDK.DTOs;
 public class AuthResponseDto
 {
     public string AccessToken { get; set; } = null!;
-    [JsonIgnore]
     public string RefreshToken { get; set; } = null!;
     public string Username { get; set; } = null!;
     public DateTime ExpiresAt { get; set; }
