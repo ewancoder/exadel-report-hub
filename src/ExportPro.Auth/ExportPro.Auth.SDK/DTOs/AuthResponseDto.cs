fbd60635eaf87d2a59ed3617614ed97b4d5a525e
BachiMjavanadze <mjavanadzebachi@mail.com>
2025-04-07 09:43:35 +0400

    return back `[JsonIgnore]`

--- src/ExportPro.Auth/ExportPro.Auth.SDK/DTOs/AuthResponseDto.cs
+++ src/ExportPro.Auth/ExportPro.Auth.SDK/DTOs/AuthResponseDto.cs
@@ -5,6 +5,7 @@ namespace ExportPro.Auth.SDK.DTOs;
 public class AuthResponseDto
 {
     public string AccessToken { get; set; } = null!;
+    [JsonIgnore]
     public string RefreshToken { get; set; } = null!;
     public string Username { get; set; } = null!;
     public DateTime ExpiresAt { get; set; }
