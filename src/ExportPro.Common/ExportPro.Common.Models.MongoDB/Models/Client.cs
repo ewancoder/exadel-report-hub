f2c19d9ca0732ef23dfef11a53e32cacde2c46d7
GiorgiUer <guffera2005@gmail.com>
2025-04-10 10:24:27 +0400

    updated models

--- src/ExportPro.Common/ExportPro.Common.Models.MongoDB/Models/Client.cs
+++ src/ExportPro.Common/ExportPro.Common.Models.MongoDB/Models/Client.cs
@@ -8,5 +8,9 @@ public class Client:IModel
     [BsonId]
     public ObjectId Id { get; set; } 
     public string Name { get; set; }
-    public ICollection<ObjectId> InvoiceIds { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public ICollection<ObjectId>? InvoiceIds { get; set; }
 }
--- src/ExportPro.Common/ExportPro.Common.Models.MongoDB/Models/Invoice.cs
+++ src/ExportPro.Common/ExportPro.Common.Models.MongoDB/Models/Invoice.cs
@@ -3,17 +3,18 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace ExportPro.Common.Models.MongoDB.Models;
 
-public class Invoice:IModel
+public class Invoice : IModel
 {
     [BsonId]
     public ObjectId Id { get; set; }
+    public string InvoiceNumber { get; set; }
     public DateTime IssueDate { get; set; }
-    public DateTime DueDate { get; set; } 
+    public DateTime DueDate { get; set; }
     public decimal Amount { get; set; }
     public string Currency { get; set; }
     public string PaymentStatus { get; set; }
-    public string InvoiceNumber { get; set; }
     public string BankAccountNumber { get; set; }
     public string Status { get; set; }
-    public ObjectId ClientId { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)] 
+    public string ClientId { get; set; }
 }
