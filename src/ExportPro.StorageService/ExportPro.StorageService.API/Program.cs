1d65bf0f26d05d636a9b4ee90d347043281928ea
GiorgiUer <guffera2005@gmail.com>
2025-04-13 23:50:31 +0400

    fixed error

--- src/ExportPro.StorageService/ExportPro.StorageService.API/Program.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using ExportPro.Auth.SDK.Interfaces;
 using ExportPro.AuthService.Configuration;
 using ExportPro.Common.DataAccess.MongoDB.Configurations;
+using ExportPro.Common.DataAccess.MongoDB.Interfaces;
 using ExportPro.Common.Shared.Behaviors;
 using ExportPro.Common.Shared.Extensions;
 using ExportPro.Common.Shared.Middlewares;
@@ -9,6 +10,7 @@ using ExportPro.StorageService.CQRS.Commands.Customer;
 using ExportPro.StorageService.DataAccess.Interfaces;
 using ExportPro.StorageService.DataAccess.Repositories;
 using ExportPro.StorageService.DataAccess.Services;
+using ExportPro.StorageService.Models.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -44,7 +46,8 @@ builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<
 builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddCommonRegistrations();
 builder.Services.AddScoped<ClientRepository>();
-
+builder.Services.AddScoped<IRepository<Invoice>>(
+    provider => provider.GetRequiredService<IInvoiceRepository>());
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
