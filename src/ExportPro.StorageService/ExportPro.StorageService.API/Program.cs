590303086dc1bd58bb4a0f6aabde5a724b1a920b
GiorgiUer <guffera2005@gmail.com>
2025-04-16 23:53:53 +0400

    deleting unused code

--- src/ExportPro.StorageService/ExportPro.StorageService.API/Program.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.API/Program.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-using System.Text;
 using ExportPro.Auth.SDK.Interfaces;
 using ExportPro.AuthService.Configuration;
 using ExportPro.Common.DataAccess.MongoDB.Configurations;
@@ -9,17 +7,13 @@ using ExportPro.Common.Shared.Behaviors;
 using ExportPro.Common.Shared.Extensions;
 using ExportPro.Common.Shared.Middlewares;
 using ExportPro.StorageService.CQRS;
-using ExportPro.StorageService.CQRS.Handlers.Client;
 using ExportPro.StorageService.DataAccess.Interfaces;
 using ExportPro.StorageService.DataAccess.Repositories;
 using ExportPro.StorageService.Models.Models;
 using ExportPro.StorageService.Validations.Validations;
 using FluentValidation;
 using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Refit;
-
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Host.UseSharedSerilogAndConfiguration();
@@ -30,49 +24,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerServices("ExportPro Storage Service");
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-builder
-    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret)),
-            ValidateIssuer = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidateAudience = true,
-            ValidAudience = jwtSettings.Audience,
-            ClockSkew = TimeSpan.Zero,
-        };
-    });
 builder.Services.AddValidatorsFromAssembly(typeof(CreateClientCommandValidator).Assembly);
 builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 builder.Services.AddAutoMapper(typeof(ExportPro.StorageService.CQRS.Profiles.MappingProfile));
-builder.Services.AddScoped<CustomerRepository>();
 builder.Services.AddCommonRegistrations();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 builder.Services.AddScoped<IRepository<Invoice>>(provider => provider.GetRequiredService<IInvoiceRepository>());
 builder.Services.AddScoped<ICollectionProvider, DefaultCollectionProvider>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddCQRS();
-builder.Services.AddRefitClient<IAuth>().ConfigureHttpClient(c => c.BaseAddress = new Uri("http://authservice:8080"));
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseMiddleware<ErrorHandlingMiddleware>();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
