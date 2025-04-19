b1171624ecaf0c81bfb193f2ac23fefefc423c94
GiorgiUer <guffera2005@gmail.com>
2025-04-19 18:42:11 +0400

    unit testing for client controller

--- src/ExportPro.StorageService/ExportPro.StorageService.Tests/ExportPro.StorageService.API/Controllers/ClientControllerTests.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.Tests/ExportPro.StorageService.API/Controllers/ClientControllerTests.cs
@@ -9,9 +9,7 @@ using FluentValidation.Results;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
-
 namespace ExportPro.StorageService.Tests.ExportPro.StorageService.API.Controllers;
-
 [TestFixture]
 public class ClientControllerTests
 {
@@ -40,9 +38,10 @@ public class ClientControllerTests
         _mediator.Send(Arg.Is<CreateClientCommand>(obj => obj.Clientdto.Equals(clientDto))).Returns(validationModel);
         var result = await _clientController.CreateClient(clientDto);
         var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
         Assert.That(objectResult?.StatusCode, Is.EqualTo(201));
     }
-
     [Test]
     public async Task CreateClient_WithInvalidClientDto_ReturnsBadRequest()
     {
@@ -58,6 +57,179 @@ public class ClientControllerTests
         _mediator.Send(Arg.Is<CreateClientCommand>(obj => obj.Clientdto.Equals(clientDto))).Returns(validationModel);
         var result = await _clientController.CreateClient(clientDto);
         var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
+    }
+    [Test]
+    public async Task GetClients_WithInvalidQueries_ReturnsBadRequest()
+    {
+        int top = 5;
+        int skip = -1;
+        var validationResult = Substitute.For<ValidationResult>();
+        _mediator.Send(Arg.Is<GetClientsQuery>(obj => obj.top == top && obj.skip == skip))
+            .Returns(new BaseResponse<ValidationModel<List<ClientResponse>>>
+            {
+                Data = new(validationResult),
+                ApiState = HttpStatusCode.BadRequest,
+                IsSuccess = false,
+            });
+        var result = await _clientController.GetClients(top, skip);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<List<ClientResponse>>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
+    }
+    [Test]
+    public async Task GetClients_WithValidQueries_ReturnsOkResult()
+    {
+        int top = 5;
+        int skip = 0;
+        var clientResponse = Substitute.For<List<ClientResponse>>();
+        _mediator.Send(Arg.Is<GetClientsQuery>(obj => obj.top == top && obj.skip == skip))
+            .Returns(new BaseResponse<ValidationModel<List<ClientResponse>>>
+            {
+                Data = new(clientResponse),
+                ApiState = HttpStatusCode.OK,
+                IsSuccess = true,
+            });
+        var result = await _clientController.GetClients(top, skip);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<List<ClientResponse>>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(200));
+    }
+    [Test]
+    public async Task GetClientById_WithValidClientId_ReturnsOkResult()
+    {
+        string clientId = "6803a0ac085ca8a4984681f4";
+        var clientResponse = Substitute.For<ClientResponse>();
+        _mediator.Send(Arg.Is<GetClientByIdQuery>(obj => obj.Id == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(clientResponse),
+                ApiState = HttpStatusCode.OK,
+                IsSuccess = true,
+            });
+        var result = await _clientController.GetClientById(clientId);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(200));
+    }
+    [Test]
+    public async Task GetClientById_WithInvalidClientId_ReturnsBadRequest()
+    {
+        string clientId = "invalidia";
+        var validationResult = Substitute.For<ValidationResult>();
+        _mediator.Send(Arg.Is<GetClientByIdQuery>(obj => obj.Id == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(validationResult),
+                ApiState = HttpStatusCode.BadRequest,
+                IsSuccess = false,
+            });
+        var result = await _clientController.GetClientById(clientId);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
+    }
+    [Test]
+    public async Task UpdateClient_WithValidClientId_ReturnsOkResult()
+    {
+        string clientId = "6803a0ac085ca8a4984681f4";
+        ClientUpdateDto clientUpdateDto = Substitute.For<ClientUpdateDto>();
+        var clientResponse = Substitute.For<ClientResponse>();
+        _mediator.Send(Arg.Is<UpdateClientCommand>(obj => obj.clientUpdateDto.Equals(clientUpdateDto) && obj.ClientId == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(clientResponse),
+                ApiState = HttpStatusCode.OK,
+                IsSuccess = true,
+            });
+        var result = await _clientController.UpdateClient(clientId, clientUpdateDto);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(200));
+    }
+    [Test]
+    public async Task UpdateClient_WithInvalidClientId_ReturnsBadRequest()
+    {
+        string clientId = "invalidia";
+        ClientUpdateDto clientUpdateDto = Substitute.For<ClientUpdateDto>();
+        var validationResult = Substitute.For<ValidationResult>();
+        _mediator.Send(Arg.Is<UpdateClientCommand>(obj => obj.clientUpdateDto.Equals(clientUpdateDto) && obj.ClientId == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(validationResult),
+                ApiState = HttpStatusCode.BadRequest,
+                IsSuccess = false,
+            });
+        var result = await _clientController.UpdateClient(clientId, clientUpdateDto);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
+    }
+    [Test]
+    public async Task UpdateClient_WithValidClientIdAndInvalidName_ReturnsBadReqeust()
+    {
+        string clientId = "6803a0ac085ca8a4984681f4";
+        ClientUpdateDto clientUpdateDto = new()
+        {
+            Name ="",
+            Description = "Test Description",
+        };
+        var validationResult = Substitute.For<ValidationResult>();
+        _mediator.Send(Arg.Is<UpdateClientCommand>(obj => obj.clientUpdateDto.Equals(clientUpdateDto) && obj.ClientId == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(validationResult),
+                ApiState = HttpStatusCode.BadRequest,
+                IsSuccess = false,
+            });
+        var result = await _clientController.UpdateClient(clientId, clientUpdateDto);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
+    }
+    [Test]
+    public async Task SoftDeleteClient_WithValidClientId_ReturnsOkResult()
+    {
+        string clientId = "6803a0ac085ca8a4984681f4";
+        var clientResponse = Substitute.For<ClientResponse>();
+        _mediator.Send(Arg.Is<SoftDeleteClientCommand>(obj => obj.ClientId == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(clientResponse),
+                ApiState = HttpStatusCode.OK,
+                IsSuccess = true,
+            });
+        var result = await _clientController.SoftDeleteClient(clientId);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
+        Assert.That(objectResult?.StatusCode, Is.EqualTo(200));
+    }
+    [Test]
+    public async Task SoftDeleteClient_WithInvalidClientId_ReturnsBadRequest()
+    {
+        string clientId = "invalidia";
+        var validationResult = Substitute.For<ValidationResult>();
+        _mediator.Send(Arg.Is<SoftDeleteClientCommand>(obj => obj.ClientId == clientId))
+            .Returns(new BaseResponse<ValidationModel<ClientResponse>>
+            {
+                Data = new(validationResult),
+                ApiState = HttpStatusCode.BadRequest,
+                IsSuccess = false,
+            });
+        var result = await _clientController.SoftDeleteClient(clientId);
+        var objectResult = result as ObjectResult;
+        Assert.That(objectResult?.Value, Is.Not.Null);
+        Assert.That(objectResult?.Value, Is.TypeOf<BaseResponse<ValidationModel<ClientResponse>>>());
         Assert.That(objectResult?.StatusCode, Is.EqualTo(400));
     }
 }
