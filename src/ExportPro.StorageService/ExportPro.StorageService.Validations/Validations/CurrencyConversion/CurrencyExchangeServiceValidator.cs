c09443aec2402aad3db1a8f962b46f9395314778
GiorgiUer <guffera2005@gmail.com>
2025-04-21 20:26:19 +0400

    corrected

--- src/ExportPro.StorageService/ExportPro.StorageService.Validations/Validations/CurrencyConversion/CurrencyExchangeServiceValidator.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.Validations/Validations/CurrencyConversion/CurrencyExchangeServiceValidator.cs
@@ -1,33 +1,33 @@
-﻿//using ExportPro.StorageService.Models.Models;
-//using ExportPro.StorageService.SDK.Services;
-//using FluentValidation;
+﻿using ExportPro.StorageService.Models.Models;
+using ExportPro.StorageService.SDK.Services;
+using FluentValidation;
 
-//namespace ExportPro.StorageService.Validations.Validations.CurrencyConversion;
+namespace ExportPro.StorageService.Validations.Validations.CurrencyConversion;
 
-//public sealed class CurrencyExchangeServiceValidator : AbstractValidator<CurrenyExchangeModel>
-//{
-//    public CurrencyExchangeServiceValidator(ICurrencyExchangeService currencyExchangeService)
-//    {
-//        RuleFor(x => x.From)
-//            .MustAsync(async (from, _) =>
-//            {
+public sealed class CurrencyExchangeServiceValidator : AbstractValidator<CurrenyExchangeModel>
+{
+    public CurrencyExchangeServiceValidator(ICurrencyExchangeService currencyExchangeService)
+    {
+        //RuleFor(x => x.From)
+        //    .MustAsync(async (from, _) =>
+        //    {
 
-//                var currenyExchangeModel = new CurrenyExchangeModel
-//                {
-//                    From = from,
-//                    To = "EUR",
-//                    Date = new DateTime(2024, 04, 17)
-//                };
-//                try
-//                {
-//                    var res = await currencyExchangeService.ExchangeRate(currenyExchangeModel);
-//                }
-//                catch (Exception ex)
-//                {
-//                    return false;
-//                }
-//                return true;
-//            }).WithMessage(x => $"Currency [{x.From}] is not supported by the  European Central Bank for conversion.");
+        //        var currenyExchangeModel = new CurrenyExchangeModel
+        //        {
+        //            From = from,
+        //            To = "EUR",
+        //            Date = new DateTime(2024, 04, 17)
+        //        };
+        //        try
+        //        {
+        //            var res = await currencyExchangeService.ExchangeRate(currenyExchangeModel);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            return false;
+        //        }
+        //        return true;
+        //    }).WithMessage(x => $"Currency [{x.From}] is not supported by the  European Central Bank for conversion.");
 
-//    }
-//}
+    }
+}
