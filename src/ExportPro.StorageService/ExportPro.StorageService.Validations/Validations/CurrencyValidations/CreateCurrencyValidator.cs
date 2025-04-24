8e80e8110feabfe45ceed8b17d75b263d961314e
sanjar1999 <sanjar.work@gmail.com>
2025-04-24 15:43:37 +0500

    some

---
+++ src/ExportPro.StorageService/ExportPro.StorageService.Validations/Validations/CurrencyValidations/CreateCurrencyValidator.cs
@@ -0,0 +1,23 @@
+﻿using ExportPro.StorageService.Models.Models;
+using FluentValidation;
+
+namespace ExportPro.StorageService.Validations.Validations.CurrencyValidations;
+
+public class CreateCurrencyValidator : AbstractValidator<Currency>
+{
+    public CreateCurrencyValidator()
+    {
+        RuleFor(x => x.CurrencyCode)
+            .NotEmpty().WithMessage("Currency code is required.")
+            .Length(3, 5).WithMessage("Currency code must be between 3 and 5 characters.");
+
+        RuleFor(x => x.CreatedAt)
+            .NotEmpty().WithMessage("CreatedAt is required.")
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("CreatedAt cannot be in the future.");
+
+        RuleFor(x => x.UpdatedAt)
+            .Must((currency, updatedAt) =>
+                !updatedAt.HasValue || updatedAt >= currency.CreatedAt)
+            .WithMessage("UpdatedAt cannot be earlier than CreatedAt.");
+    }
+}
