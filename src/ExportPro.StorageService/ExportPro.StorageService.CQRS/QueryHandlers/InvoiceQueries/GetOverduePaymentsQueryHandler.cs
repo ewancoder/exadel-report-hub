6e3c463e611f4ee93b1ca70bf8499a5576fc462e
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-05-04 17:57:19 +0400

    overdue payments

---
+++ src/ExportPro.StorageService/ExportPro.StorageService.CQRS/QueryHandlers/InvoiceQueries/GetOverduePaymentsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using ExportPro.Common.Shared.Library;
+using ExportPro.Common.Shared.Mediator;
+using ExportPro.StorageService.DataAccess.Interfaces;
+using ExportPro.StorageService.SDK.Responses;
+
+namespace ExportPro.StorageService.CQRS.QueryHandlers.InvoiceQueries;
+
+
+public record GetOverduePaymentsQuery: IQuery<OverduePaymentsResponse>;
+
+public sealed class GetOverduePaymentsQueryHandler(IInvoiceRepository invoiceRepository) : IQueryHandler<GetOverduePaymentsQuery, OverduePaymentsResponse>
+{
+    public async Task<BaseResponse<OverduePaymentsResponse>> Handle(GetOverduePaymentsQuery request, CancellationToken cancellationToken)
+    {
+        var overdueInvoices = await invoiceRepository.GetOverdueInvoices(cancellationToken);
+        if(overdueInvoices == null || overdueInvoices.Count == 0)
+            return new BadRequestResponse<OverduePaymentsResponse>("No invoices issued in selected period.");
+        var result = new OverduePaymentsResponse
+        {
+            OverdueInvoicesCount = overdueInvoices.Count,
+            TotalOverdueAmount = overdueInvoices.Sum(x => x.Amount)
+        };
+        return new SuccessResponse<OverduePaymentsResponse>(result);
+    }
+}
+
