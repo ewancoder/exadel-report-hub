d3ed1e0b1d7a6a8b22a4d68ec02085f4cd5ca98f
GiorgiUer <guffera2005@gmail.com>
2025-05-05 00:25:07 +0400

    delete unused methods

--- src/ExportPro.StorageService/ExportPro.StorageService.CQRS/Extensions/ObjectIdConvertExtension.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.CQRS/Extensions/ObjectIdConvertExtension.cs
@@ -12,23 +12,7 @@ public static class ObjectIdConvertExtension
         return new Guid(guidBytes);
     }
 
-    public static Guid? ToGuidEvenNull(this ObjectId objectId)
-    {
-        var objectIdBytes = objectId.ToByteArray();
-        var guidBytes = new byte[16];
-        Array.Copy(objectIdBytes, 0, guidBytes, 0, objectIdBytes.Length);
-        return new Guid(guidBytes);
-    }
-
     public static ObjectId ToObjectId(this Guid guid)
-    {
-        var guidBytes = guid.ToByteArray();
-        var objectIdBytes = new byte[12];
-        Array.Copy(guidBytes, 0, objectIdBytes, 0, objectIdBytes.Length);
-        return new ObjectId(objectIdBytes);
-    }
-
-    public static ObjectId? ToObjectIdEvenNull(this Guid guid)
     {
         var guidBytes = guid.ToByteArray();
         var objectIdBytes = new byte[12];
