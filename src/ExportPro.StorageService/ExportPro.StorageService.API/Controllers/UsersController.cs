59928a1caad71f450c82eb5a5c2f8a6e0320af89
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-05-11 23:23:44 +0400

    add users controller

---
+++ src/ExportPro.StorageService/ExportPro.StorageService.API/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExportPro.StorageService.API.Controllers;
+
+
+[Route("api/users/")]
+[Authorize]
+public class UsersController
+{
+
+}
+
