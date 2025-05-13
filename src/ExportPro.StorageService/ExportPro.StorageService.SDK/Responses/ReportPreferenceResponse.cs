237d52ee0e1ebea97d972b08e4a6c36984eec241
GiorgiUer <guffera2005@gmail.com>
2025-05-13 22:45:32 +0400

    merged sanjars changes

--- src/ExportPro.StorageService/ExportPro.StorageService.SDK/Responses/ReportPreferenceResponse.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.SDK/Responses/ReportPreferenceResponse.cs
@@ -1,8 +1,9 @@
-﻿using ExportPro.StorageService.Models.Enums;
+﻿using ExportPro.Common.Models.MongoDB;
+using ExportPro.StorageService.Models.Enums;
 
 namespace ExportPro.StorageService.SDK.Responses;
 
-public sealed class ReportPreferenceResponse
+public sealed class ReportPreferenceResponse : AuditModel
 {
     public required Guid Id { get; set; }
     public required Guid UserId { get; set; }
@@ -11,9 +12,5 @@ public sealed class ReportPreferenceResponse
     public ReportFormat ReportFormat { get; set; }
     public required string CronExpression { get; set; }
     public string? HumanReadableSchedule { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public string? UpdatedBy { get; set; }
-    public required string CreatedBy { get; set; }
     public bool IsEnabled { get; set; }
 }
