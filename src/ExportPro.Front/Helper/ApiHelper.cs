3d632d4ca114b39b7327fd54ad38513ed9d67855
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-05-16 18:11:27 +0400

    add basic modal  and client helper

---
+++ src/ExportPro.Front/Helper/ApiHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+
+namespace ExportPro.Front.Helper
+{
+    public class ApiHelper(HttpClient httpClient)
+    {
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>()
+                   ?? throw new InvalidOperationException("Response was null");
+        }
+
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string url, TRequest data)
+        {
+            var response = await httpClient.PostAsJsonAsync(url, data);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>()
+                   ?? throw new InvalidOperationException("Response was null");
+        }
+
+        public async Task<TResponse> PutAsync<TRequest, TResponse>(string url, TRequest data)
+        {
+            var response = await httpClient.PutAsJsonAsync(url, data);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>()
+                   ?? throw new InvalidOperationException("Response was null");
+        }
+
+        public async Task DeleteAsync(string url)
+        {
+            var response = await httpClient.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
--- src/ExportPro.Front/Program.cs
+++ src/ExportPro.Front/Program.cs
@@ -4,12 +4,21 @@ using ExportPro.Front;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using ExportPro.Front.Services;
+using ExportPro.Front.Helper;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped(sp =>
+{
+    var httpClient = new HttpClient
+    {
+        BaseAddress = new Uri("http://localhost:1200/") 
+    };
+    return new ApiHelper(httpClient);
+});
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
