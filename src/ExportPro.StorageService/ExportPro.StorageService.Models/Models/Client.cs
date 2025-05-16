0e2d8ef650eadbbce01dec0189a555b534566b1a
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-05-16 21:57:12 +0400

    remove unused stuff

--- src/ExportPro.StorageService/ExportPro.StorageService.Models/Models/Client.cs
+++ src/ExportPro.StorageService/ExportPro.StorageService.Models/Models/Client.cs
@@ -10,7 +10,5 @@ public sealed class Client : AuditModel, IModel
     public bool IsDeleted { get; set; } = false;
     public List<Item>? Items { get; set; } = [];
     public List<Plans>? Plans { get; set; } = [];
-    public Guid? OwnerId { get; set; }
-    public List<Guid>? ClientAdmins { get; set; } = [];
     public ObjectId Id { get; set; }
 }
