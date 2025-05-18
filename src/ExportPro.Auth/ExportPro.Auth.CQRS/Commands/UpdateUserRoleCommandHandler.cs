8bd5437bb0faca35c2dbd6790d6c2ad51d019e24
AnniSmiles <73358289+AnniSmiles@users.noreply.github.com>
2025-05-18 13:58:11 +0400

    add permissions

--- src/ExportPro.Auth/ExportPro.Auth.CQRS/Commands/UpdateUserRoleCommandHandler.cs
+++ src/ExportPro.Auth/ExportPro.Auth.CQRS/Commands/UpdateUserRoleCommandHandler.cs
@@ -1,13 +1,20 @@
 ﻿using ExportPro.Auth.SDK.Models;
 using ExportPro.AuthService.Services;
 using ExportPro.Common.Shared.Enums;
+using ExportPro.Common.Shared.Extensions;
+using ExportPro.Common.Shared.Helpers;
 using ExportPro.Common.Shared.Library;
 using ExportPro.Common.Shared.Mediator;
 using MongoDB.Bson;
 
 
 namespace ExportPro.Auth.CQRS.Commands;
-public record UpdateUserRoleCommand(ObjectId UserId, ObjectId ClientId, UserRole NewRole) : ICommand<bool>;
+public record UpdateUserRoleCommand(ObjectId UserId, ObjectId ClientId, UserRole NewRole) : ICommand<bool>, IPermissionedRequest
+{
+    public List<Guid>? ClientIds => [ClientId.ToGuid()];
+    public Resource Resource => Resource.Users;
+    public CrudAction Action => CrudAction.Update;
+};
 
 public class UpdateUserRoleCommandHandler(IACLService aclService) : ICommandHandler<UpdateUserRoleCommand, bool>
 {
--- src/ExportPro.Auth/ExportPro.Auth.CQRS/Queries/GetUserClientPermissionsQueryHandler.cs
+++ src/ExportPro.Auth/ExportPro.Auth.CQRS/Queries/GetUserClientPermissionsQueryHandler.cs
@@ -4,10 +4,18 @@ using ExportPro.Common.Shared.Mediator;
 using MongoDB.Bson;
 using ExportPro.AuthService.Services;
 using ExportPro.Auth.SDK.Models;
+using ExportPro.Common.Shared.Helpers;
+using ExportPro.Common.Shared.Extensions;
+using ExportPro.Common.Shared.Enums;
 
 namespace ExportPro.Auth.CQRS.Queries;
 
-public record GetUserClientPermissionsQuery(ObjectId UserId, ObjectId ClientId): IQuery<List<PermissionDTO>>;
+public record GetUserClientPermissionsQuery(ObjectId UserId, ObjectId ClientId): IQuery<List<PermissionDTO>>, IPermissionedRequest
+{
+    public List<Guid>? ClientIds => ClientId != default ? [ClientId.ToGuid()] : null;
+    public Resource Resource => Resource.Users;
+    public CrudAction Action => CrudAction.Read;
+};
 
 
 public class GetUserClientPermissionsQueryHandler(IACLService aclService) : IQueryHandler<GetUserClientPermissionsQuery, List<PermissionDTO>>
